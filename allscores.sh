740702c268b953fa9eb7fe4b5be79a057a3e0350
campbell000 <ac.simpledex@gmail.com>
2019-05-09 22:50:58 -0400

    more stuff

---
+++ allscores.sh
@@ -0,0 +1,11 @@
+python scoring.py --emb ~/Desktop/embeddings/walks_40_path_40/flickr_nobacktracking --network example_graphs/flickr.mat --num-shuffle 20
+
+python scoring.py --emb ~/Desktop/embeddings/walks_40_path_40/flickr_self_avoiding --network example_graphs/flickr.mat --num-shuffle 20
+
+python scoring.py --emb ~/Desktop/embeddings/walks_40_path_40/flickr_mbrw2 --network example_graphs/flickr.mat --num-shuffle 20
+
+python scoring.py --emb ~/Desktop/embeddings/walks_40_path_40/flickr_mbrw10 --network example_graphs/flickr.mat --num-shuffle 20
+
+python scoring.py --emb ~/Desktop/embeddings/walks_40_path_40/flickr_mbrw100 --network example_graphs/flickr.mat --num-shuffle 20
+
+python scoring.py --emb ~/Desktop/embeddings/walks_40_path_40/flickr_mbrw1000 --network example_graphs/flickr.mat --num-shuffle 20
